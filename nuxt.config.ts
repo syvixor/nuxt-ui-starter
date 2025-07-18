cb5528a6e2af9d1a6a0eec90c150ece5871995df
syvixor <syvixor@proton.me>
2025-07-18 14:27:06 +0100

    chore: rename global css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
     modules: ["@nuxt/ui"],
     devtools: { enabled: false },
     colorMode: { preference: "dark" },
-    css: ["~/assets/css/main.css"]
+    css: ["~/assets/css/global.css"]
 });
